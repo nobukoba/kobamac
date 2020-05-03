035ccd644870c27eb0d38180a62e83db8418e3f8
nobukoba <n.kobayashi.phd@gmail.com>
2020-05-04 05:02:48 +0900

    KOBAMAC_DIR is automatically set

--- root/base/ini_kobamac.C
+++ root/base/ini_kobamac.C
@@ -61,17 +61,18 @@ void ini_kobamac(){
     gSystem->Load("lib/libAllGrutinizer.so");
   }
 
-
-  TString cmd = Form("echo $(cd %s && pwd)", gEnv->GetValue("KOBAMAC_DIR","."));
-  TString s = gSystem->GetFromPipe(cmd.Data());
-  gEnv->SetValue("KOBAMAC_DIR",s.Data());
+  TString str = __FILE__; /* preprocessor command __FILE__ returns the file name of the file itself */ 
+  std::cout << __FILE__ << std::endl;
+  str.Resize(str.Length()-13); /* Remove the string "ini_kobamac.C" at the last */
+  TString cmd = Form("echo $(cd %s && pwd)", str.Data());
+  str = gSystem->GetFromPipe(cmd.Data());
+  str.Resize(str.Length()-10); /* Remove the string "/root/base" at the last */
+  gEnv->SetValue("KOBAMAC_DIR",str.Data());
   std::cout <<"ini_kobamac.C: KOBAMAC_DIR=" << gEnv->GetValue("KOBAMAC_DIR","") << std::endl;
   
   cmd = Form("find %s -type d -not -path '*/\\.*' | tr -d '\r' | tr '\n' ':' | sed -e 's/:$//'",gEnv->GetValue("KOBAMAC_DIR","."));
-  s = gSystem->GetFromPipe(cmd.Data());
-  s.Prepend(":");
-  s.Prepend(gROOT->GetMacroPath());
-  gROOT->SetMacroPath(s.Data());
+  str = gSystem->GetFromPipe(cmd.Data());
+  gROOT->SetMacroPath(Form("%s:%s", gROOT->GetMacroPath(), str.Data()));
   gROOT->ProcessLine(Form(".L %s/root/base/TBrowserEx.C+",   gEnv->GetValue("KOBAMAC_DIR",".")));
   gROOT->ProcessLine("TBrowserEx *gBrowserEx = 0;");
   gROOT->ProcessLine(Form(".L %s/root/base/tbr.C",           gEnv->GetValue("KOBAMAC_DIR",".")));
--- root/base/rootlogon_example.C
+++ root/base/rootlogon_example.C
@@ -1,5 +1,4 @@
 {
-  gEnv->SetValue("KOBAMAC_DIR","$HOME/kobamac");
   gROOT->ProcessLine(".x $HOME/kobamac/root/base/ini_kobamac.C");
   gROOT->ProcessLine("new TBrowserEx();"); /* Auto launching of the TBrowerEx. This line can be commented out. */ 
 }
