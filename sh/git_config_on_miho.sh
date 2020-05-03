2c35c8d59ffbb4bd4388d333cf35a81b66165d1c
nobukoba <n.kobayashi.phd@gmail.com>
2020-05-03 20:35:00 +0900

    sh python dir were removed

--- sh/git_config_on_miho.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git config --global http.proxy socks5://localhost:8080
-git config --global https.proxy socks5://localhost:8080
-git config -l
-ssh -fND 8080 kobayash@koba-desktop
--- sh/qdel_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cmd="qstat | grep -i kobayash | awk '{print \$1}' | xargs qdel"
-echo $cmd
-eval $cmd
