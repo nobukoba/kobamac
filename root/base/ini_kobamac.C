679e5139a5f5586e3c16f62f186cee11ed7a057f
nobukoba <n.kobayashi.phd@gmail.com>
2020-05-04 05:26:26 +0900

    gStyle should be written in rootlogon.C

--- root/base/ini_kobamac.C
+++ root/base/ini_kobamac.C
@@ -1,66 +1,8 @@
 #include "TROOT.h"
 #include "TEnv.h"
-#include "TStyle.h"
 #include "TSystem.h"
 
 void ini_kobamac(){
-  gEnv->SetValue("Print.Command","lpr -P%p");
-  //gEnv->SetValue("Print.Printer","-Pa3c4476 -o PageSize=A4 -o PageRegion=A4");
-  gEnv->SetValue("Print.Printer","a3c4476");
-  gEnv->SetValue("Print.FileType","ps");
-  gEnv->SetValue("Unix.*.Root.UseTTFonts","false"); // If TTFonts are disabled, the TH2::SetShowProjectionX() will causes segv.
-  //gEnv->SetValue("Unix.*.Root.UseTTFonts","true");
-  //gEnv->SetValue("Unix.*.Root.TTFontPath","");
-  gEnv->SetValue("Canvas.ShowEventStatus", "false");
-  gEnv->SetValue("Canvas.ShowToolTips", "true");
-  gEnv->SetValue("Canvas.ShowToolBar", "true");
-  gEnv->SetValue("Canvas.ShowEditor", "false");
-  
-  gROOT->SetStyle("Plain");
-  gStyle->SetHistFillColor(0);
-  gStyle->SetHistLineColor(kBlue);
-  gStyle->SetFuncColor(kRed);
-  gStyle->SetFrameLineWidth(1);
-  gStyle->SetPadGridX(0);
-  gStyle->SetPadGridY(0);
-  gStyle->SetPadColor(0);
-  gStyle->SetPadBorderMode(1);
-  gStyle->SetCanvasColor(0);
-  gStyle->SetTitleFillColor(0);
-  gStyle->SetTitleStyle(0);
-  gStyle->SetStatColor(0);
-  gStyle->SetStatStyle(0);
-  gStyle->SetStatX(0.9);  
-  gStyle->SetStatY(0.9);
-  gStyle->SetStatH(0.4);
-  gStyle->SetStatW(0.4);
-  gStyle->SetPalette(1);
-  //gStyle->SetOptLogz(1);
-  //gStyle->SetOptTitle(0);
-  gStyle->SetOptStat(1111111);
-  gStyle->SetOptFit(1);
-  gStyle->SetOptDate(1);
-  gStyle->SetEndErrorSize(0);
-  gStyle->SetPaperSize(20,26);
-  gStyle->SetNdivisions(505,"X");
-  gStyle->SetLabelFont(132,"XYZ");
-  gStyle->SetTitleFont(132,"XYZ");
-  gStyle->SetTitleFont(132,"");
-  gStyle->SetTextFont(132);
-  gStyle->SetStatFont(132);
-  gStyle->GetAttDate()->SetTextFont(132);
-  gStyle->SetTitleSize(0.04,"XYZ");
-  gStyle->SetTitleSize(0.04,"");
-  gStyle->SetLabelSize(0.04,"XYZ");
-  gStyle->SetTextSize(0.04);
-  gStyle->SetStatFontSize(0.04);
-  gStyle->SetTitleFontSize(0.04);  
-  /*gROOT->ForceStyle();*/
-  
-  if (gSystem->AccessPathName("lib/libAllGrutinizer.so")==0) {
-    gSystem->Load("lib/libAllGrutinizer.so");
-  }
-
   TString str = __FILE__; /* preprocessor command __FILE__ returns the file name of the file itself */ 
   /* std::cout << __FILE__ << std::endl; */
   str.Resize(str.Length()-13); /* Remove the string "ini_kobamac.C" at the last */
