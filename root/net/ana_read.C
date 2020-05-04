f6ebc17c7238b195c4112e79c504ac43149bed49
nobukoba <n.kobayashi.phd@gmail.com>
2020-05-05 05:57:15 +0900

    ResetActBrowser() in ana_read.C

--- root/net/ana_read.C
+++ root/net/ana_read.C
@@ -15,6 +15,9 @@ void ana_read(){
   Info("fastMergeServer","Receive input from client %d for %s",clientId,filename.Data());
   TMemFile* oldfile = (TMemFile*) gROOT->GetListOfFiles()->FindObject(filename.Data());
   if (oldfile) {
+    if (gROOT->ProcessLine("gBrowserEx; 2> /dev/null")) {
+      gROOT->ProcessLine("gBrowserEx->ResetActBrowser();");
+    }
     gROOT->GetListOfFiles()->Remove(oldfile);
     delete oldfile;
   }
