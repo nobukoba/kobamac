54edd835f725179d6b0163e38c5c1e862306c20a
nobukoba <n.kobayashi.phd@gmail.com>
2020-05-05 05:48:01 +0900

    init_tbr.C was fixed

--- root/base/init_tbr.C
+++ root/base/init_tbr.C
@@ -1,9 +1,8 @@
 void init_tbr(){
   if(!gROOT->GetClass("TBrowserEx")){
     TString str = __FILE__;
-    str.Resize(str.Length()-7); /* Remove the string "/tbr.C" in the tail */
-    /*gROOT->ProcessLine(Form(".x %s/ini_kobamac.C", str.Data()));*/
-    gROOT->Macro(Form("%s/ini_kobamac.C", str.Data()));
+    str.Resize(str.Length()-11); /* Remove the string "/init_tbr.C" in the tail */
+    gROOT->ProcessLine(Form(".x %s/init_kobamac.C", str.Data()));
   }
   gROOT->ProcessLine("tbr();");
 }
