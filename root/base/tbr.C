aa2167910e3b05f4febccbebf6e8190f675fa60a
nobukoba <n.kobayashi.phd@gmail.com>
2020-05-05 05:00:52 +0900

    tbr.C fixed for PATH

--- root/base/tbr.C
+++ root/base/tbr.C
@@ -1,8 +1,8 @@
 void tbr(){
   if(!gROOT->GetClass("TBrowserEx")){
     TString str = __FILE__;
-    str.Resize(str.Length()-5); /* Remove the string "tbr.C" in the tail */
-    gROOT->ProcessLine(Form(".x %s/ini_kobamac.C;", str.Data()));
+    str.Resize(str.Length()-6); /* Remove the string "/tbr.C" in the tail */
+    gROOT->ProcessLine(Form(".x %s/ini_kobamac.C", str.Data()));
   }
   gROOT->ProcessLine("new TBrowserEx();");
 }
