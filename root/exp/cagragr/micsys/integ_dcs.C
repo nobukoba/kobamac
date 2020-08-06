56a0d1905452b9ba00569d0c3f8626f2ab04f20e
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-08-07 08:36:06 +0900

    modified

--- root/exp/cagragr/micsys/integ_dcs.C
+++ root/exp/cagragr/micsys/integ_dcs.C
@@ -35,9 +35,12 @@ Double_t dcs_fuc_2_cut(Double_t *x, Double_t *p){
       (B < -0.060)||
       (A < theta_s_in_rad-0.020)||
       (A > theta_s_in_rad+0.020)||
-      (B > -2.5*(A- theta_s_in_rad)+0.1)      
+      (B > -9./4.*(A- theta_s_in_rad)+0.075)||    
+      (B >  9./4.*(A- theta_s_in_rad)+0.075)||
+      (B <  9./4.*(A- theta_s_in_rad)-0.075)||
+      (B < -9./4.*(A- theta_s_in_rad)-0.075)
       ) {
-    ret = 1.;
+    ret = 0.;
   }
   return ret;
   
@@ -49,13 +52,13 @@ void integ_dcs(){
   gr[1] = new TGraph("dcs_pZr94E80_num.dat");
   //gr[0]->Draw("a*");
   TF1 *f  = new TF1("f",dcs_fuc,0,180,0);
-  TF2 *f2 = new TF2("f2",dcs_fuc_2,-pi/8., pi/8., -pi/8., pi/8.,0);
-  TF2 *f2_cut = new TF2("f2_cut",dcs_fuc_2_cut,-pi/8., pi/8., -pi/8., pi/8.,0);
+  TF2 *f2 = new TF2("f2",dcs_fuc_2,0., pi/16., -pi/16., pi/16.,0);
+  TF2 *f2_cut = new TF2("f2_cut",dcs_fuc_2_cut,0., pi/16., -pi/16., pi/16.,0);
   f2->SetNpx(400);
   f2->SetNpy(400);
   f2_cut->SetNpx(400);
   f2_cut->SetNpy(400);
-  f2_cut->Draw("col");
+  f2->Draw("col");
   TLine *l = new TLine();
   l->SetLineColor(1);
   l->SetLineWidth(1);
@@ -63,18 +66,26 @@ void integ_dcs(){
   l->DrawLine(theta_s_in_rad+0.02, -0.03, theta_s_in_rad+0.02,  0.03);
   l->DrawLine(theta_s_in_rad+0.02,  0.03, theta_s_in_rad-0.02,  0.03);
   l->DrawLine(theta_s_in_rad-0.02,  0.03, theta_s_in_rad-0.02, -0.03);
+
+  double x1 = theta_s_in_rad-0.02;
+  double x2 = theta_s_in_rad+0.02;
+  double y1 = -0.06;
+  double y2 = 0.06;
   
-  l->DrawLine(theta_s_in_rad-0.02, -0.06, theta_s_in_rad+0.02, -0.06);
+  //l->DrawLine(x1,0., x1, 9./4.*(x1- theta_s_in_rad)+0.075);
+  //l->DrawLine(x1, 0.03,-0.004+theta_s_in_rad,0.06);
+  //l->DrawLine(-0.004+theta_s_in_rad,0.06,0.004+theta_s_in_rad,0.06);
+  //l->DrawLine(0.004+theta_s_in_rad,0.06,x2,0.03);
+  l->DrawLine(theta_s_in_rad-0.02, -0.06, theta_s_in_rad+0.02,  -0.06);
   l->DrawLine(theta_s_in_rad+0.02, -0.06, theta_s_in_rad+0.02,  0.06);
   l->DrawLine(theta_s_in_rad+0.02,  0.06, theta_s_in_rad-0.02,  0.06);
   l->DrawLine(theta_s_in_rad-0.02,  0.06, theta_s_in_rad-0.02, -0.06);
   
-  
-//  std::cout << "f2->Integral/2.4 mrad: " <<
-//    f2_cut->Integral(theta_s_in_rad-0.02,theta_s_in_rad+0.02,-0.03, 0.03) / (0.04 * 0.06) << std::endl;
-//  
-//  std::cout << "f2->Integral/4.8 mrad: " <<
-//    f2_cut->Integral(theta_s_in_rad-0.02,theta_s_in_rad+0.02,-0.05, 0.05) / (0.04 * 0.10) << std::endl;
+//
+//4 * 0.06) << std::endl;
+//
+//
+//4 * 0.10) << std::endl;
   
   return;
 }
