7a3e6067cb635cf0262e04f0045153139e8fb651
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-08-13 17:52:55 +0900

    open file

--- root/exp/cagragr/tree/chk_run2214_20200813.C
+++ root/exp/cagragr/tree/chk_run2214_20200813.C
@@ -1,5 +1,9 @@
+
 void chk_run2214_20200813(){
   std::cout << "Macro: chk_run2214_2020813.C" << std::endl;
+  TFile *f = TFile::Open("/home/kobayash/mnt/GRUTinizer/output/date20200813/run2214/hist_MakeGRTree_Nobu_20200811_drftok_fin_q_nogrut.root");
+  TTree *tr = (TTree *)f->Get("tr");
+  tr->Print();
   
   return;
 }
