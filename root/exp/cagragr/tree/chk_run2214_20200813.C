a997fe4ec71d2911bccb59f6c689339737148ec3
nobukoba <n.kobayashi.phd@gmail.com>
2020-08-13 22:48:15 -0400

    chk_run was updated

--- root/exp/cagragr/tree/chk_run2214_20200813.C
+++ root/exp/cagragr/tree/chk_run2214_20200813.C
@@ -1,5 +1,8 @@
 void chk_run2214_20200813(){
   std::cout << "Macro: chk_run2214_2020813.C" << std::endl;
-  
+  TFile *f = TFile::Open("hist_MakeGRTree_Nobu_20200801_chk_q_nogrut.root");
+  TTree *tr = (TTree *)f->Get("tr");
+  tr->Print();
+
   return;
 }
