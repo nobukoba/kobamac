ce8dcbe7fa1f9f5249d86d5d48f02445f492d882
nobukoba <n.kobayashi.phd@gmail.com>
2020-08-15 10:25:20 -0400

    multi_sliders.C was complieted

--- root/exp/cagragr/tree/multi_sliders.C
+++ root/exp/cagragr/tree/multi_sliders.C
@@ -12,46 +12,55 @@
 #include "TGTextEntry.h"
 #include "TGSlider.h"
 
-#define NSLIDERS 2
-
 class MultiHSliders : public TGMainFrame {
   RQ_OBJECT("MultiHSliders")
-  private:
+private:
+  Int_t               nsliders;
   TGLayoutHints       *fBly, *fBfly;
-  TGHorizontalFrame   *fHframe[NSLIDERS];
-  TGHSlider           *fHslider[NSLIDERS];
-  TGTextEntry         *fTeh[NSLIDERS];
-  TGTextBuffer        *fTbh[NSLIDERS];
-  Double_t            fSliderMin[NSLIDERS];
-  Double_t            fSliderMax[NSLIDERS];
-  Double_t            fSliderPich[NSLIDERS];
-  Int_t               HId[NSLIDERS];
-  Int_t               HSId[NSLIDERS];
-  Double_t            SliderValues[NSLIDERS];
+  TGHorizontalFrame   **fHframe;
+  TGHSlider           **fHslider;
+  TGTextEntry         **fTeh;
+  TGTextBuffer        **fTbh;
+  Double_t            *fSliderMin;
+  Double_t            *fSliderMax;
+  Double_t            *fSliderPich;
+  Int_t               *HId;
+  Int_t               *HSId;
 public:
-  MultiHSliders();
+  MultiHSliders(Int_t);
   virtual ~MultiHSliders();
+  void SetMinMax(Int_t, Double_t, Double_t);
   void CloseWindow();
-  void DoText(const char *text);
+  void DoText(const char *);
   void DoSlider();
   void DoFunction();
-  Double_t* GetVal();
+  Double_t  GetVal(Int_t);
   ClassDef(MultiHSliders, 1)
 };
 
-/*______________________________________________________________________________*/
-MultiHSliders::MultiHSliders() : TGMainFrame(gClient->GetRoot(), 100, 100)
+/*____________________________________________________________________________*/
+MultiHSliders::MultiHSliders(Int_t nsli) :
+  TGMainFrame(gClient->GetRoot(), 100, 100), nsliders(nsli)
 {
-  char buf[32];
+  fHframe      = new TGHorizontalFrame*[nsliders];
+  fHslider     = new TGHSlider*[nsliders];
+  fTeh         = new TGTextEntry*[nsliders];
+  fTbh         = new TGTextBuffer*[nsliders];
+  fSliderMin   = new Double_t[nsliders];
+  fSliderMax   = new Double_t[nsliders];
+  fSliderPich  = new Double_t[nsliders];
+  HId          = new Int_t[nsliders];
+  HSId         = new Int_t[nsliders];
+
   SetCleanup(kDeepCleanup);
   /*--- layout for buttons: top align, equally expand horizontally */
   fBly = new TGLayoutHints(kLHintsTop | kLHintsExpandX, 5, 5, 5, 5);
   /*--- layout for the frame: place at bottom, right aligned */
   fBfly = new TGLayoutHints(kLHintsTop | kLHintsRight, 5, 5, 5, 5);
-
-  for (Int_t islider = 0; islider < NSLIDERS; islider++) {
+  
+  for (Int_t islider = 0; islider < nsliders; islider++) {
     HId[islider]  = islider;
-    HSId[islider] = islider + NSLIDERS;
+    HSId[islider] = islider + nsliders;
     fHframe[islider] = new TGHorizontalFrame(this, 0, 0, 0);
     fHslider[islider] = new TGHSlider(fHframe[islider], 500,
 				      kSlider1 | kScaleDownRight, HSId[islider],
@@ -62,14 +71,10 @@ MultiHSliders::MultiHSliders() : TGMainFrame(gClient->GetRoot(), 100, 100)
     fHslider[islider]->SetRange(0,1000);
     fHslider[islider]->SetPosition(500);
     fHframe[islider]->Resize(200, 25);
-    fSliderMin[islider] = -10000000.;
-    fSliderMax[islider] =  10000000.;
-    fSliderPich[islider] = (fSliderMax[islider] - fSliderMin[islider])/1000;
-    
-    fTeh[islider] = new TGTextEntry(fHframe[islider], fTbh[islider] = new TGTextBuffer(5), HId[islider]);
+    fTbh[islider] = new TGTextBuffer(5);
+    fTeh[islider] = new TGTextEntry(fHframe[islider], fTbh[islider], HId[islider]);
+    SetMinMax(islider, 0., 1.);
     fTeh[islider]->SetToolTipText("Pointer Position Value");
-    sprintf(buf, "%.5g", fSliderPich[islider]*fHslider[islider]->GetPosition()+fSliderMin[islider]);
-    fTbh[islider]->AddText(0, buf);
     fTeh[islider]->Connect("TextChanged(char*)", "MultiHSliders", this,
 			   "DoText(char*)");
     fHframe[islider]->AddFrame(fHslider[islider], fBly);
@@ -79,34 +84,58 @@ MultiHSliders::MultiHSliders() : TGMainFrame(gClient->GetRoot(), 100, 100)
   
   /* Set main frame name, map sub windows (buttons), initialize layout
      algorithm via Resize() and map main frame */
-  SetWindowName(Form("MultiSlider: %d slider(s)", NSLIDERS));
+  SetWindowName(Form("MultiSlider: %d slider(s)", nsliders));
   MapSubwindows();
   Resize(GetDefaultSize());
   MapWindow();
   DoFunction();
 }
 
-/*______________________________________________________________________________*/
+/*____________________________________________________________________________*/
+void MultiHSliders::SetMinMax(Int_t islider, Double_t min, Double_t max)
+{
+  if (islider >= nsliders) {
+    std::cout << "islider: " << islider << " is larger than or equal to nsliders: " << nsliders << std::endl;
+    return;
+  }
+  fSliderMin[islider] = min;
+  fSliderMax[islider] = max;
+  fSliderPich[islider] = (fSliderMax[islider] - fSliderMin[islider])/1000;
+  char buf[32];
+  sprintf(buf, "%.5g", fSliderPich[islider]*fHslider[islider]->GetPosition()+fSliderMin[islider]);
+  fTbh[islider]->Clear();
+  fTbh[islider]->AddText(0, buf);
+}
+/*____________________________________________________________________________*/
 MultiHSliders::~MultiHSliders()
 {
   /* Clean up */
   Cleanup();
+  delete [] fHframe;
+  delete [] fHslider;
+  delete [] fTeh;
+  delete [] fTbh;
+  delete [] fSliderMin;
+  delete [] fSliderMax;
+  delete [] fSliderPich;
+  delete [] HId;
+  delete [] HSId;
 }
 
-/*______________________________________________________________________________*/
+/*____________________________________________________________________________*/
 void MultiHSliders::CloseWindow()
 {
   /* Called when window is closed via the window manager. */
   delete this;
 }
 
-/*______________________________________________________________________________*/
+/*____________________________________________________________________________*/
 void MultiHSliders::DoText(const char * /*text*/)
 {
   /* Handle text entry widgets. */
   TGTextEntry *te = (TGTextEntry *) gTQSender;
   Int_t id = te->WidgetId();
-  for (Int_t islider = 0; islider < NSLIDERS; islider++) {
+  for (Int_t islider = 0; islider < nsliders; islider++) {
     if (id == HId[islider]) {
       fHslider[islider]->SetPosition((int)((atof(fTbh[islider]->GetString())-fSliderMin[islider])/fSliderPich[islider]));
       break;
@@ -115,7 +144,7 @@ void MultiHSliders::DoText(const char * /*text*/)
   DoFunction();
 }
 
-/*______________________________________________________________________________*/
+/*____________________________________________________________________________*/
 void MultiHSliders::DoSlider()
 {
   /* Handle slider widgets. */
@@ -124,9 +153,9 @@ void MultiHSliders::DoSlider()
   Int_t id = sl->WidgetId();
   /* std::cout << "gTQSender: " << ((TObject*) gTQSender)->GetName() << std::endl;
      std::cout << "id: " << id << std::endl; */
-  for (Int_t islider = 0; islider < NSLIDERS; islider++) {
+  for (Int_t islider = 0; islider < nsliders; islider++) {
     if (id == HSId[islider]) {
-      sprintf(buf, "%.3f", fSliderPich[islider]*fHslider[islider]->GetPosition()+fSliderMin[islider]);
+      sprintf(buf, "%.5g", fSliderPich[islider]*fHslider[islider]->GetPosition()+fSliderMin[islider]);
       /* std::cout << "buf: " << buf << std::endl; */
       fTbh[islider]->Clear();
       fTbh[islider]->AddText(0, buf);
@@ -138,20 +167,20 @@ void MultiHSliders::DoSlider()
   }
   DoFunction();
 }
-/*______________________________________________________________________________*/
+/*____________________________________________________________________________*/
 void MultiHSliders::DoFunction() {
   Emit("DoFunction()");
 }
-/*______________________________________________________________________________*/
-Double_t* MultiHSliders::GetVal() {
-  for (Int_t islider = 0; islider < NSLIDERS; islider++) {
-    SliderValues[islider] = 
-      fSliderPich[islider]*fHslider[islider]->GetPosition()+fSliderMin[islider];
+/*____________________________________________________________________________*/
+Double_t MultiHSliders::GetVal(Int_t islider) {
+  if (islider >= nsliders) {
+    std::cout << "islider: " << islider << " is larger than or equal to nsliders: " << nsliders << std::endl;
+    return 0.;
   }
-  return SliderValues;
+  return fSliderPich[islider]*fHslider[islider]->GetPosition()+fSliderMin[islider];
 }
 
-/*______________________________________________________________________________*/
+/*____________________________________________________________________________*/
 class HistoManager {
 private:
   TTree    *tr;
@@ -167,8 +196,8 @@ public:
   ClassDef(HistoManager, 1)
 };
 
-/*______________________________________________________________________________*/
-HistoManager::HistoManager(MultiHSliders * shs)
+/*____________________________________________________________________________*/
+HistoManager::HistoManager(MultiHSliders * mhs)
 {
   /* TFile *f = TFile::Open("/home/kobayash/mnt/GRUTinizer/output/date20200813/run2214/hist_MakeGRTree_Nobu_20200811_drftok_fin_q.root");*/
   TFile *f = TFile::Open("./hist_MakeGRTree_Nobu_20200811_drftok_fin_q.root");
@@ -184,8 +213,8 @@ HistoManager::HistoManager(MultiHSliders * shs)
   tr->SetBranchAddress("de2",     &de2);
   canvas = new TCanvas("cV3D","PolyLine3D & PolyMarker3D Window",200,10,500,500);
   hist = new TH2D ("h","h",200, -1000., 1000, 200, -300., 300.);
-  shs->Connect("DoFunction()", "HistoManager", this, "DrawHistos()");
-  shs->DoFunction();
+  mhs->Connect("DoFunction()", "HistoManager", this, "DrawHistos()");
+  mhs->DoFunction();
 }
 HistoManager::~HistoManager(){}
 
@@ -194,9 +223,9 @@ void HistoManager::DrawHistos()
   double d2r = 3.141592653589793238/180.;
   ULong64_t NumOfEntries = tr->GetEntries();
 
-  MultiHSliders *shs = (MultiHSliders *) gTQSender;
-  Double_t a = shs->GetVal()[0];
-  Double_t z = shs->GetVal()[1];
+  MultiHSliders *mhs = (MultiHSliders *) gTQSender;
+  Double_t a = mhs->GetVal(0);
+  Double_t z = mhs->GetVal(1);
   std::cout << "tr->GetEntries(): " << NumOfEntries << std::endl;
   std::cout << "a: " << a << std::endl;
   std::cout << "z: " << z << std::endl;
@@ -220,7 +249,7 @@ void HistoManager::DrawHistos()
 
 void multi_sliders()
 {
-  std::cout << "NSLIDERS: " << NSLIDERS << std::endl;
-  MultiHSliders *shs = new MultiHSliders();
-  new HistoManager(shs);
+  MultiHSliders *mhs = new MultiHSliders(10);
+  mhs->SetMinMax(1,-10000.,10000.);
+  new HistoManager(mhs);
 }
