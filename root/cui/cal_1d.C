329b267e14552cd91fbaa5c37fd03d2a9ef03dfc
nobukoba <n.kobayashi.phd@gmail.com>
2020-09-22 16:20:43 +0900

    new_histo_with_suffix was added

--- root/cui/cal_1d.C
+++ root/cui/cal_1d.C
@@ -1,9 +1,30 @@
 void cal_1d(TH1* hist, Double_t a, Double_t b){
-  gROOT->ProcessLine(Form(".L %s/root/cui/new_histo_with_suffix.C", gEnv->GetValue("KOBAMAC_DIR",".")));
-  TH1 *hout = (TH1*)gROOT->ProcessLine(Form("new_histo_with_suffix((TH1*)%p,\"%s\");",hist,"_cal"));
-  hout->SetTitle(hist->GetTitle());
-  hout->GetXaxis()->SetLimits(a*hist->GetXaxis()->GetXmin()+b,
-			      a*hist->GetXaxis()->GetXmax()+b);
+  TString str;
+  Int_t num = 1;
+  while (1) {
+    str = Form("%s_cal%d",hist->GetName(),suffix,num);
+    if (gROOT->Get(str.Data())==0) {break;}
+    num++;
+  }
+  if (hist->InheritsFrom("TH2")) {
+    std::cout << "The histogram should be 1D. Exit."<< std::endl;
+    return;
+  }
+  if (!hist->InheritsFrom("TH1")) {
+    std::cout << "The histogram is not 1D. Exit."<< std::endl;
+    return;
+  }
+  TDirectory *save = gDirectory;
+  gROOT->cd();
+  TH1* hout = new TH1D(str.Data(),hist->GetTitle(),
+		       hist->GetXaxis()->GetNbins(),
+		       a*hist->GetXaxis()->GetXmin()+b,
+		       a*hist->GetXaxis()->GetXmax()+b);
+  save->cd();
+  for (int i=0; i <= hist->GetXaxis()->GetNbins() + 1; i++){
+    hout->SetBinContent(i,hist->GetBinContent(i));
+    /*hout->SetBinError(i,hist->GetBinError(i));*/
+  }
   hout->SetEntries(hist->GetEntries());
   hout->Draw("colz");
   gPad->Modified();
