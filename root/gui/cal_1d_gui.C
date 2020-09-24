2995c408f0ad8e58113d28deb376fd683c940c25
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-09-24 15:50:12 +0900

    1172 --> 1173 in cal_1d_gui.C

--- root/gui/cal_1d_gui.C
+++ root/gui/cal_1d_gui.C
@@ -5,8 +5,9 @@ void cal_1d_gui(){
   }
   TBrowserEx *gBrowserEx = (TBrowserEx *)gROOT->ProcessLine("gBrowserEx;");
   if (gBrowserEx == 0) {return;}
-  TString str = gBrowserEx->OpenTGInputDialog("Peak energies (1173 1332 etc...)");
+  TString str = gBrowserEx->OpenTGInputDialog("Peak energies (1172 1332 etc...)");
   if (str.EqualTo("")) {return;}
+
   
   std::istringstream iss(str.Data());
   std::vector <Double_t> pars;
